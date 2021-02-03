0e9ea3dd51cf77ec1a81f17e274e49df44ee0c6c
ezarwiratama <77718063+ezarwiratama@users.noreply.github.com>
2021-02-03 15:25:54 +0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Navbar from '../Components/Navbar'
+import Header from '../components/header'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      <Header/>
     </div>
   )
 }
