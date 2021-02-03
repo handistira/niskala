c4394ee5ad836721f8e81fec7afc4d39bccee38d
ezarwiratama <ezar.hardin10@gmail.com>
2021-02-03 15:45:29 +0700

    menambahkan components

---
+++ components/contents.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Contents() {
+    return (
+        <div >
+            <h1 className="text-7xl">Lorem Ipsum</h1>
+            <div className="flex justify-between">
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default Contents
