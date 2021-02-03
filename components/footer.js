3ab6f9940f160e2a7469d1163a710220c1622bcb
ezarwiratama <77718063+ezarwiratama@users.noreply.github.com>
2021-02-03 15:29:13 +0700

    menambahkan components
    header, contents, footer, navbar

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function footer() {
+    return (
+        <div>
+           <div>
+                <h1 className="text-4xl text-center">Niskala</h1>
+           </div>
+           <div>
+               <p className="text-center">Created By Niskala ©Copyright 2021 </p>
+           </div>
+        </div>
+    )
+}
